4b2d24f88731c281b11c7794596235719b08655d
ITkah <ilatkacenko132@gmail.com>
2019-12-25 12:05:46 +0200

    modul done

--- router.js
+++ router.js
@@ -1,6 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
 module.exports = function () {
-    const router = express.Router();
     router.get('/', (req,res) => {
         res.sendFile('index.html');
-    });
+    })
+    return router;
 };
